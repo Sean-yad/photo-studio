d8cd489ac6f2f7f3314221de8b60d49ad32748fe
Brian Plotner <95835591+PlacidHouse@users.noreply.github.com>
2022-03-21 19:50:27 -0500

    New Changes to About Page...
    text change....

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,7 +1,10 @@
 import React from "react";
 
 function About() {
-  return <div>THIS IS THE ABOUT PAGE</div>;
+  return <div>THIS IS THE ABOUT PAGE
+    <h1>*This is a change to the page!</h1>
+  </div>;
+  
 }
 
 export default About;
