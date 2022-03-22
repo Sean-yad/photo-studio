7faa78062bdf8f5775b5310a2a2c087bf4d08ff3
Brian Plotner <95835591+PlacidHouse@users.noreply.github.com>
2022-03-21 19:53:52 -0500

    Newest change to About page...
    text <p> change...

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -3,6 +3,7 @@ import React from "react";
 function About() {
   return <div>THIS IS THE ABOUT PAGE
     <h1>*This is a change to the page!</h1>
+    <p>This is a new change to the About page.</p>
   </div>;
   
 }
