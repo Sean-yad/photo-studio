195be97e7ebcb422beff4a33e32a589f7e0fc17c
Sean-yad <94640179+Sean-yad@users.noreply.github.com>
2022-03-21 19:58:35 -0500

    test change

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,13 +1,16 @@
 import React from "react";
 
 function About() {
-  return <div>THIS IS THE ABOUT PAGE
-    <h1>*This is a change to the page!</h1>
-    <p>This is a new change to the About page.</p>
-    <br></br>
-    <p>And this is another change.</p>
-  </div>;
-  
+  return (
+    <div>
+      THIS IS THE ABOUT PAGE
+      <h1>*This is a change to the page!</h1>
+      <p>This is a new change to the About page.</p>
+      <br></br>
+      <p>And this is another change.</p>
+      <p>what it do my friend!.</p>
+    </div>
+  );
 }
 
 export default About;
