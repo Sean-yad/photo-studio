9d0ee6b018ae7276311b5b7d7bf86ff6fa792e52
Brian Plotner <95835591+PlacidHouse@users.noreply.github.com>
2022-03-21 19:58:00 -0500

    Latest Change to About page
    new <p> added...

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -4,6 +4,8 @@ function About() {
   return <div>THIS IS THE ABOUT PAGE
     <h1>*This is a change to the page!</h1>
     <p>This is a new change to the About page.</p>
+    <br></br>
+    <p>And this is another change.</p>
   </div>;
   
 }
