a9405363c7f9abc960f0eddc2e2ed101aa652ff9
Sean-yad <94640179+Sean-yad@users.noreply.github.com>
2022-03-28 13:09:48 -0500

    some basic changes

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,20 @@
 import React from "react";
+import "./CSS/Style.css";
+import "./images/mountain.jpg";
 
 function Home() {
-  return <div>THIS IS HOME PAGE</div>;
+  return (
+    <>
+      <div className="hero-title">
+        <img src="./images/mountain.jpg" className="hero-image" alt="" />
+        <div className="hero-text">
+          <h1>I am John Doe</h1>
+          <p>And I'm a Photographer</p>
+          <button>Hire me</button>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Home;
