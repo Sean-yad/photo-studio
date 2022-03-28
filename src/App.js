7936d74449fcfbfdd1671458ed877ecf67fe8a38
Sean-yad <94640179+Sean-yad@users.noreply.github.com>
2022-03-28 17:13:19 -0500

    color issue

--- src/App.js
+++ src/App.js
@@ -6,43 +6,56 @@ import Portfolio from "./Pages/Portfolio";
 import Blog from "./Pages/Blog";
 import Contact from "./Pages/Contact";
 import ErrorPage from "./Pages/ErrorPage";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core"
+import { purple } from "@material-ui/core/colors"
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#fefefe",
+    },
+    secondary: purple,
+  },
+});
 
 function App() {
   return (
-    <Router>
-      <nav className="flex-container">
-        <h1 className="logo">
-          <Link to="/"> Logo </Link>
-        </h1>
-        <ul className="navigation">
-          <li>
-            <Link to="/"> Home </Link>
-          </li>
-          <li>
-            <Link to="/Portfolio"> Portfolio </Link>
-          </li>
-          <li>
-            <Link to="/Blog"> Blog </Link>
-          </li>
-          <li>
-            <Link to="/About"> About</Link>
-          </li>
-          <li>
-            <Link to="/Contact"> Contact </Link>
-          </li>
-        </ul>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/About" element={<About />} />
-        <Route path="/Portfolio" element={<Portfolio />} />
-        <Route path="/Blog" element={<Blog />} />
-        <Route path="/About" element={<About />} />
-        <Route path="/Contact" element={<Contact />} />
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
-      <div> Footer </div>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <nav className="flex-container">
+          <h1 className="logo">
+            <Link to="/"> Logo </Link>
+          </h1>
+          <ul className="navigation">
+            <li>
+              <Link to="/"> Home </Link>
+            </li>
+            <li>
+              <Link to="/Portfolio"> Portfolio </Link>
+            </li>
+            <li>
+              <Link to="/Blog"> Blog </Link>
+            </li>
+            <li>
+              <Link to="/About"> About</Link>
+            </li>
+            <li>
+              <Link to="/Contact"> Contact </Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Portfolio" element={<Portfolio />} />
+          <Route path="/Blog" element={<Blog />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+        <div> Footer </div>
+      </Router>
+    </ThemeProvider>
   );
 }
 
--- src/Pages/Components/button.js
+++ src/Pages/Components/button.js
@@ -2,9 +2,10 @@ import * as React from "react";
 import Button from "@mui/material/Button";
 import "./component.css";
 
+
 export default function DisableElevation() {
   return (
-    <Button variant="contained" disableElevation className="Button">
+    <Button variant="contained" disableElevation>
       Disable elevation
     </Button>
   );
