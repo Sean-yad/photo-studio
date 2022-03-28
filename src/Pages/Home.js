b087c38976075ba57b9bc596676ad6ab75849325
Brian Plotner <95835591+PlacidHouse@users.noreply.github.com>
2022-03-28 14:22:15 -0500

    adjusted hero to background image

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,13 +1,12 @@
 import React from "react";
 import "./CSS/Style.css";
-import "./images/mountain.jpg";
+import background from "./images/mountain.jpg";
 
 function Home() {
   return (
     <>
-      <div className="hero-title">
-        <img src="./images/mountain.jpg" className="hero-image" alt="" />
-        <div className="hero-text">
+      <div className="hero-title" style={{ backgroundImage: `url(${background})` }}>
+        <div className="hero-text" >
           <h1>I am John Doe</h1>
           <p>And I'm a Photographer</p>
           <button>Hire me</button>
