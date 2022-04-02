5f59e737b2335d18c1f7eb41b9388c4b0e89d3d1
Sean-yad <94640179+Sean-yad@users.noreply.github.com>
2022-04-02 14:06:50 -0500

    footer

--- src/App.js
+++ src/App.js
@@ -7,65 +7,55 @@ import Blog from "./Pages/Blog";
 import Contact from "./Pages/Contact";
 import ErrorPage from "./Pages/ErrorPage";
 
-
 function App() {
   return (
-    
-      <Router>
-        <nav className="flex-container">
-          <h1 className="logo">
-            <Link to="/"> Logo </Link>
-          </h1>
-          <ul className="navigation">
-            <li>
-              <Link to="/"> Home </Link>
-            </li>
-            <li>
-              <Link to="/Portfolio"> Portfolio </Link>
-            </li>
-            <li>
-              <Link to="/Blog"> Blog </Link>
-            </li>
-            <li>
-              <Link to="/About"> About</Link>
-            </li>
-            <li>
-              <Link to="/Contact"> Contact </Link>
-            </li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/About" element={<About />} />
-          <Route path="/Portfolio" element={<Portfolio />} />
-          <Route path="/Blog" element={<Blog />} />
-          <Route path="/About" element={<About />} />
-          <Route path="/Contact" element={<Contact />} />
-          <Route path="*" element={<ErrorPage />} />
-        </Routes>
-        <div class="block_footer">
-            <div class="container">
-                <footer>
-                    <div class="row">
-                        <div class="col">
-                        </div>
-                    </div>
-                    
-                    <div class="row_two">
-                        <div class="col"><a href="https://www.facebook.com"><img src="images/facebook_logo35px.png" alt="fb logo" /></a></div>
-                        <div class="col"><a href="https://www.instagram.com"><img src="images/instagram_logo35px.png" alt="ig logo" /></a></div>
-                    </div>
-
-                </footer>
-            </div>
+    <Router>
+      <nav className="flex-container">
+        <h1 className="logo">
+          <Link to="/"> Illumine Studio</Link>
+        </h1>
+        <ul className="navigation">
+          <li>
+            <Link to="/"> Home </Link>
+          </li>
+          <li>
+            <Link to="/Portfolio"> Portfolio </Link>
+          </li>
+          <li>
+            <Link to="/Blog"> Blog </Link>
+          </li>
+          <li>
+            <Link to="/About"> About</Link>
+          </li>
+          <li>
+            <Link to="/Contact"> Contact </Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Portfolio" element={<Portfolio />} />
+        <Route path="/Blog" element={<Blog />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+      <footer>
+        <div class="wrapper">
+          <small>
+            &copy;2022 <strong>Illumine Studio</strong>, All Rights Reserved
+          </small>
+          <nav className="footer-nav">
+            <a href="#">Home</a>
+            <a href="#">Portfolio</a>
+            <a href="#">Blog</a>
+            <a href="#">About</a>
+            <a href="#">Contact</a>
+          </nav>
         </div>
-        <div class="block_copyright">
-            <div class="container">
-                <h4>©2021 Some Guy</h4>
-            </div>
-        </div>
-      </Router>
-   
+      </footer>
+    </Router>
   );
 }
 
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -18,16 +18,18 @@ function Home() {
       </div>
       <div className="about_block">
         <div>
-        <h1>This is my Home Page!</h1>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi alias, 
-                            odit voluptatibus dolores odio impedit, molestias veniam delectus et sapiente 
-                            eligendi. Quaerat reiciendis unde accusamus? Sit perferendis ut, molestias accusamus 
-                            molestiae natus cum placeat consectetur quisquam, doloribus nostrum at! Fuga ab facere 
-                            cumque voluptatem. Aliquid, repellat! Quis totam impedit perferendis. Sit, non. Dolores 
-                            blanditiis modi necessitatibus libero. Dignissimos, animi temporibus.
-                        </p>
+          <h1>This is my Home Page!</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi
+            alias, odit voluptatibus dolores odio impedit, molestias veniam
+            delectus et sapiente eligendi. Quaerat reiciendis unde accusamus?
+            Sit perferendis ut, molestias accusamus molestiae natus cum placeat
+            consectetur quisquam, doloribus nostrum at! Fuga ab facere cumque
+            voluptatem. Aliquid, repellat! Quis totam impedit perferendis. Sit,
+            non. Dolores blanditiis modi necessitatibus libero. Dignissimos,
+            animi temporibus.
+          </p>
         </div>
-
       </div>
     </>
   );
