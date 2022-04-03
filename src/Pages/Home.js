8ca8689ef5eb66009861eff24ef1b30660a5154b
Sean-yad <94640179+Sean-yad@users.noreply.github.com>
2022-04-03 17:04:35 -0500

    Flexbox added (temporary design)

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,14 +1,16 @@
 import React from "react";
 import "./CSS/Style.css";
-import background from "./images/mountain.jpg";
+import heroimage from "./images/mountain.jpg";
+import panda from "./images/pandablue.jpg";
 import Button from "./Components/button";
 
 function Home() {
   return (
     <>
+      {/* hero section start*/}
       <div
         className="hero-title"
-        style={{ backgroundImage: `url(${background})` }}
+        style={{ backgroundImage: `url(${heroimage})` }}
       >
         <div className="hero-text">
           <h1>yo soy brian</h1>
@@ -16,21 +18,27 @@ function Home() {
           <Button />
         </div>
       </div>
-      <div className="about_block">
-        <div>
-          <h1>This is my Home Page!</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi
-            alias, odit voluptatibus dolores odio impedit, molestias veniam
-            delectus et sapiente eligendi. Quaerat reiciendis unde accusamus?
-            Sit perferendis ut, molestias accusamus molestiae natus cum placeat
-            consectetur quisquam, doloribus nostrum at! Fuga ab facere cumque
-            voluptatem. Aliquid, repellat! Quis totam impedit perferendis. Sit,
-            non. Dolores blanditiis modi necessitatibus libero. Dignissimos,
-            animi temporibus.
-          </p>
+      {/* hero section end*/}
+
+      {/* flexbox content start*/}
+      <section className="landing-page-home">
+        <div className="landing-container-home">
+          <div
+            className="image-container-home"
+            style={{ backgroundImage: `url(${panda})` }}
+          ></div>
+          <div className="text-container-home">
+            <h2>Text Container!</h2>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Cupiditate vero aliquam debitis architecto neque nostrum id
+              incidunt fuga, obcaecati tempora! Quae recusandae dolorem nam
+              dicta soluta quibusdam architecto officiis sit.
+            </p>
+          </div>
         </div>
-      </div>
+      </section>
+      {/* flexbox content end*/}
     </>
   );
 }
